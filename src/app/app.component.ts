7d492f9bd9a834ac2bec155a27695dae2fa34e7d
gmateoc <pas_ul15@hotmail.com>
2023-02-18 14:25:12 -0500

    se agrega event biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularRegistroEmpleados';
+  titulo = 'Lista de Empleados';
+
+  empleados:Empleado[]=[
+    new Empleado("Juan","Diaz","Presidente",7500),
+    new Empleado("Fernandez","Ana","Directora",5500),
+    new Empleado("Laura","Dez","Jefa de Sección",4500),
+    new Empleado("Ana","Lopez","Administrativo",3500)
+  ];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
 }
